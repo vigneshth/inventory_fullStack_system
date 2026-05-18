d53bc63e086bc755be1c65c5d3a699527461f649
Vignesh <vigneshrpt2005@gmail.com>
2026-05-19 01:21:40 +0530

    Fixed Vercel CORS issue

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -16,7 +16,7 @@ const BODY_LIMIT = '5mb';
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000','https://frontend-inventrack.onrender.com'],
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id', 'x-user-role', 'x-user-email']
